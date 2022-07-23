35852d64e6ab8ce84bc91ef81e3384dc1fce08be
Cristian Silva <cristianeduardo.dev@gmail.com>
2022-07-23 15:52:50 -0300

    fixing passive customer list

--- multiserver/Server.java
+++ multiserver/Server.java
@@ -3,27 +3,33 @@ package multiserver;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Server {
     private static int serverPort = 59091;
-    private static ServerSocket serverSocket;
-
+    
     public static void main(String[] args) {      
         try {
-            FileWriter ordersDB = new FileWriter("orders.csv");
-            FileWriter clientsDB = new FileWriter("clients.csv");
+            new FileWriter("orders.csv");
+            new FileWriter("clients.csv");
         } catch (IOException e1) {
             e1.printStackTrace();
         }
-
+        
         try {
             int socketCounter = 0;
-            serverSocket = new ServerSocket(serverPort);
+            Map<String, Socket> sockets = new HashMap<>(); 
+            ServerSocket serverSocket = new ServerSocket(serverPort);
             
             while (true) {
                 socketCounter++;
+                Socket socket = serverSocket.accept();
 
-                new ClientHandler(serverSocket.accept(), socketCounter).start();
+                sockets.put(String.valueOf(socketCounter), socket);
+
+                new ClientHandler(socket, socketCounter).start();
             }
         } catch (IOException e) {
             if(e.getMessage().contains("Connection reset")) {
